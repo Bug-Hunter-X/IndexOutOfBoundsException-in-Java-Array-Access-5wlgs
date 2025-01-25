94f94d5d474a56341287e45854b45e115af0158c
BugHunter-X <askzenai@gmail.com>
2025-01-25 16:36:05 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        try {
+            int index = 5; 
+            if(index >= 0 && index < arr.length){
+                System.out.println(arr[index]);
+            } else {
+                System.out.println("Index out of bounds");
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
